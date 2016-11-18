e0d37c6d52295747fcca5008640031e5cb068096
Storm Witziers <stormwitziers123@gmail.com>
2016-11-18 09:06:21 +0100

    Revert "Revert "The music bot works now and is ready to be tested. (See desc for more info)""
    This reverts commit bda1109d21e1fc3ea2c1e304843b5e43f1c34516.

--- GestaltBot/Modules/MusicModule.cs
+++ GestaltBot/Modules/MusicModule.cs
@@ -6,6 +6,7 @@ using Discord.Audio;
 using GestaltBot.Enums;
 
 using NAudio;
+using NAudio.FileFormats.Map;
 using NAudio.Wave;
 using NAudio.CoreAudioApi;
 
@@ -21,7 +22,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.IO;
-using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace GestaltBot.Modules {
@@ -29,8 +30,14 @@ namespace GestaltBot.Modules {
 
         private ModuleManager m_manager;
         private DiscordClient m_client;
-        private IAudioClient m_audioClient;
+
+        private Discord.Channel m_voiceChannel;
         private ReturnYoutubeInfo m_linkInfo;
+        private Queue<string> m_songQueue = new Queue<string>();
+
+        private bool m_isplaying;
+
+        private IAudioClient m_audioClient;
 
         void IModule.Install(ModuleManager manager) {
 
@@ -42,6 +49,7 @@ namespace GestaltBot.Modules {
             });
 
             AudioService voiceservice = m_client.GetService<AudioService>();
+
 
             manager.CreateCommands("", cmd => {
 
@@ -58,45 +66,49 @@ namespace GestaltBot.Modules {
                     string servername = e.Server.Name;
                     string channelname = e.User.VoiceChannel.Name;
 
-                    try {
+                    m_voiceChannel = m_client.FindServers(servername).FirstOrDefault().FindChannels(channelname).FirstOrDefault();
 
-                        Discord.Channel voicechannel = m_client.FindServers(servername).FirstOrDefault().FindChannels(channelname).FirstOrDefault();
+                    try {
 
                         await GetYoutubeLink(e.Args[0]);
                         videos = m_linkInfo.videos;
-                        Console.WriteLine(videos[0].ToString());
-
-
-                        bool reading = false;
-                        string url = "";
-                        foreach(char readchar in videos.ToString()) {
-                            if (readchar == '(') {
-                                url = "";
-                                reading = true;
-                            }
-                            if (readchar == ')')
-                                reading = false;
-
-                            if (reading)
-                                url += readchar.ToString();
-                        }
-
+                        
+                        string url = await ReadOutLink(videos[0].ToString());
+                 
                         VideoInfo video = await DownloadYoutubeLink(url);
-                       
-
-                        if (videos != null && videos.Count != 0) {
-                            Console.WriteLine("got it: " + videos.Count);
-                        }
-                        else {
-                            await e.Channel.SendMessage(" | Sorry i couldn't find anything!");
+                        
+                        
+                        if (videos == null && url.Count() == 0) {
+                            await e.Channel.SendMessage(":no_entry:  | Sorry i couldn't find anything!");
                             return;
                         }
+                        bool playingmusicloop = false;
 
-                        m_audioClient = await voiceservice.Join(voicechannel);
-                        string path = Path.Combine("D:/downloads/", video.Title + video.AudioExtension);
-                        SendAudio(path, m_audioClient);
+                        string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), RemoveIllegalPathCharacters(video.Title) + video.AudioExtension);
+                        m_songQueue.Enqueue(path);
+                        await e.Channel.SendMessage(":cd: | This song is queued: **" + RemoveIllegalPathCharacters(video.Title) + "**");
 
-                        await e.Channel.SendMessage("| Good evenin' lads :raised_hand:");
+                        do {
+
+                            if (!m_isplaying && m_songQueue.Count > 0) {
+
+                                if (!playingmusicloop) {
+                                    m_audioClient = await voiceservice.Join(m_voiceChannel);
+                                    playingmusicloop = true;
+                                }
+
+                                path = m_songQueue.Peek();
+                                await e.Channel.SendMessage(":cd: | Now playing: **" + RemoveIllegalPathCharacters(video.Title) + "**");
+                                SendAudio(path, m_audioClient);
+                                m_songQueue.Dequeue();
+                            }
+                            else if (!m_isplaying && m_songQueue.Count == 0) {
+                                StopAudio(m_audioClient, voiceservice, e);
+                                playingmusicloop = false;
+                            }
+
+                        }
+                        while (playingmusicloop);
                     }
                     catch (Exception ex) {
                         Console.WriteLine(ex.ToString());
@@ -110,18 +122,12 @@ namespace GestaltBot.Modules {
 
                     try {
 
-                        string servername = e.Server.Name;
-                        string channelname = e.User.VoiceChannel.Name;
-                        Discord.Channel voicechannel = m_client.FindServers(servername).FirstOrDefault().FindChannels(channelname).FirstOrDefault();
-
-                        StopAudio(m_audioClient);
-                        await voiceservice.Leave(voicechannel);
-                        await e.Channel.SendMessage("| Later :raised_hand:");
+                        StopAudio(m_audioClient, voiceservice, e);
                     }
                     catch (Exception ex) {
 
                         Console.WriteLine(ex);
-                        await e.Channel.SendMessage("| Uh oh something went wrong!");
+                        await e.Channel.SendMessage(":sos: | Uh oh, something went wrong!");
                     }
 
                 });
@@ -129,11 +135,11 @@ namespace GestaltBot.Modules {
 
         }
         public void SendAudio(string filepath, IAudioClient voiceclient) {
-
+            
             int channelcount = m_client.GetService<AudioService>().Config.Channels;
             WaveFormat outformat = new WaveFormat(48000, 16, channelcount);
 
-            using (var MP3reader = new Mp3FileReader(filepath))
+            using (var MP3reader = new MediaFoundationReader(filepath))
             using (var resampler = new MediaFoundationResampler(MP3reader, outformat)) {
                 resampler.ResamplerQuality = 60;
                 int blockSize = outformat.AverageBytesPerSecond / 50;
@@ -146,14 +152,32 @@ namespace GestaltBot.Modules {
                         for (int i = byteCount; i < blockSize; i++)
                             buffer[i] = 0;
                     }
+
+                    m_isplaying = true;
                     voiceclient.Send(buffer, 0, blockSize); // Send the buffer to Discord
+                    
                 }
+                m_isplaying = false;
             }
         }
 
-        public void StopAudio(IAudioClient voiceclient) {
-            voiceclient.Clear();
-            voiceclient.Disconnect();
+        public async void StopAudio(IAudioClient voiceclient, AudioService voiceservice, CommandEventArgs e) {
+            if(voiceclient != null) {
+
+                string servername = e.Server.Name;
+                string channelname = e.Channel.Name;
+                Discord.Channel voicechannel = m_client.FindServers(servername).FirstOrDefault().FindChannels(channelname).FirstOrDefault();
+
+                m_isplaying = false;
+
+                await voiceservice.Leave(voicechannel);
+                voiceclient.Clear();
+                await voiceclient.Disconnect();
+                await e.Channel.SendMessage(":raised_hand: | See you later!");
+            }
+            else {
+                await e.Channel.SendMessage(":japanese_goblin: | I am not in your channel you dumb fuck!");
+            }
         }
 
         public async Task<bool> GetYoutubeLink(string searchargs) {
@@ -165,15 +189,14 @@ namespace GestaltBot.Modules {
 
             var searchrequest = youtubeservice.Search.List("snippet");
             searchrequest.Q = searchargs;
-
             SearchListResponse searchresponds = await searchrequest.ExecuteAsync();
 
             List<string> videos = new List<string>();
             List<string> channels = new List<string>();
             List<string> playlists = new List<string>();
 
-            foreach (var searchresult in searchresponds.Items) {
 
+            foreach (var searchresult in searchresponds.Items) {
                 switch (searchresult.Id.Kind) {
                     case "youtube#video":
                         videos.Add(string.Format("{0} ({1})", searchresult.Snippet.Title, searchresult.Id.VideoId));
@@ -190,27 +213,61 @@ namespace GestaltBot.Modules {
             return true;
         }
 
-        public async Task<VideoInfo> DownloadYoutubeLink(string url) {
+        public async Task<string> ReadOutLink(string video) {
 
-            string fullurl = Path.Combine("www.youtube.com/" + url);
+            bool reading = false;
 
-            Console.WriteLine(fullurl);
+            foreach (char readchar in video.ToString()) {
 
-            IEnumerable<VideoInfo> videoinfos = DownloadUrlResolver.GetDownloadUrls(fullurl);
+                if (readchar == '(') {
+                    video = "";
+                    reading = true;
+                }
+                if (readchar == ')')
+                    reading = false;
 
-            VideoInfo video = videoinfos
-                .Where(info => info.CanExtractAudio)
-                .OrderBy(info => info.AudioBitrate)
-                .First();
-
-            if (video.RequiresDecryption) { DownloadUrlResolver.DecryptDownloadUrl(video); }
-
-            AudioDownloader audiodownloader = new AudioDownloader(video, Path.Combine("D:/downloads/", video.Title + video.AudioExtension));
-
-            audiodownloader.DownloadProgressChanged += (sender, args) => Console.WriteLine(args.ProgressPercentage * 0.85);
-            audiodownloader.AudioExtractionProgressChanged += (sender, args) => Console.WriteLine(85 + args.ProgressPercentage * 0.15);
+                if (reading && readchar != '(')
+                    video += readchar.ToString();
+                Console.WriteLine(video);
+            }
 
             return video;
+        }
+
+        public async Task<VideoInfo> DownloadYoutubeLink(string url) {
+
+            try {
+
+                string fullurl = Path.Combine("https://www.youtube.com/watch?v=" + url);
+
+                IEnumerable<VideoInfo> videoinfos = DownloadUrlResolver.GetDownloadUrls(fullurl,false);
+
+                VideoInfo video = videoinfos
+                    .OrderByDescending(info => info.AudioBitrate)
+                    .First(info => info.VideoType == VideoType.Mp4 && info.Resolution == 360);
+
+                if (video.RequiresDecryption) { DownloadUrlResolver.DecryptDownloadUrl(video); }
+                string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                    RemoveIllegalPathCharacters(video.Title) + video.AudioExtension);
+                if (File.Exists(path)) { return video; }
+
+                VideoDownloader audiodownloader = new VideoDownloader(video, Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), 
+                    RemoveIllegalPathCharacters(video.Title) + video.AudioExtension));
+
+                    audiodownloader.DownloadProgressChanged += (sender, args) => Console.WriteLine(args.ProgressPercentage * 1);
+                    audiodownloader.Execute();
+                    return video;
+                
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex);
+                return null;
+            }
+        }
+        private static string RemoveIllegalPathCharacters(string path) {
+            string regexSearch = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
+            var r = new Regex(string.Format("[{0}]", Regex.Escape(regexSearch)));
+            return r.Replace(path, "");
         }
     }
 
