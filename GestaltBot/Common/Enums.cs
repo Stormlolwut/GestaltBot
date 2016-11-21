d6751ec457e775174763409e43ce49d9748c09ce
Storm Witziers <stormwitziers123@gmail.com>
2016-11-21 19:03:36 +0100

    Reloaded everything to fix DLL bug on github

---
+++ GestaltBot/Common/Enums.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestaltBot.Enums
+{
+
+    public enum DiscordAccesLevel
+    {
+        Blocked,
+        MemePeasant,
+        MemeKnight,
+        MemeLord,
+        MemeKing,
+        BotOwner
+    }
+}
---
+++ GestaltBot/Modules/OverwatchModule.cs
@@ -0,0 +1,45 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.Commands.Permissions.Levels;
+using Discord.Modules;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestaltBot.Modules {
+    class OverwatchModule : IModule {
+        private ModuleManager m_manager;
+        private DiscordClient m_client;
+
+
+        void IModule.Install(ModuleManager manager) {
+
+            m_manager = manager;
+            m_client = manager.Client;
+            manager.CreateCommands("", cmd => {
+
+                cmd.CreateCommand("overwatch")
+                .Alias("o")
+                .Description("Check your overwatch stats")
+                .Parameter("text", ParameterType.Unparsed)
+                .Do(async (e) => {
+
+                    string input = e.Args[0];
+                    string battletag = input.Replace('#', '-');
+
+                    try {
+                        await e.Channel.SendMessage(e.User.Mention + " | Here are your stats.");
+                        await e.Channel.SendMessage("http://masteroverwatch.com/profile/pc/eu/" + battletag);
+                    }
+                    catch (Exception ex) {
+                        await e.Channel.SendMessage(ex.ToString());
+                    }
+
+                });
+            });
+        }
+    }
+}
